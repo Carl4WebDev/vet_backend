d789f33c6ea9e180dd16db21f8b31c9f8db49066
carl4dev <carlforwebdev@gmail.com>
2025-09-20 11:45:26 +0800

    ssl/tls

--- src/infrastructure/config/db.js
+++ src/infrastructure/config/db.js
@@ -1,3 +1,21 @@
+// import pkg from "pg";
+// import ENV from "./env.js";
+
+// const { Pool } = pkg;
+
+// export const pool = new Pool({
+//   user: ENV.DB.USER,
+//   host: ENV.DB.HOST,
+//   database: ENV.DB.NAME,
+//   password: ENV.DB.PASSWORD,
+//   port: ENV.DB.PORT,
+// });
+
+// pool
+//   .connect()
+//   .then(() => console.log("✅ Connected to Postgres"))
+//   .catch((err) => console.error("❌ DB Error:", err.message));
+
 import pkg from "pg";
 import ENV from "./env.js";
 
@@ -9,6 +27,7 @@ export const pool = new Pool({
   database: ENV.DB.NAME,
   password: ENV.DB.PASSWORD,
   port: ENV.DB.PORT,
+  ssl: { rejectUnauthorized: false }, // ✅ Required for Render
 });
 
 pool
